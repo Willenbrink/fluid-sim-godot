0235f3cfc11a9d3377d5baf95e0d74d3ecd8078d
Sebastian Willenbrink <sebastian.willenbrink@gmail.com>
2024-08-21 12:43:02 +0200

    Increase camera speed

--- controls/camera.gd
+++ controls/camera.gd
@@ -1,5 +1,6 @@
-class_name FreeLookCamera extends Camera3D
+# Copied from the documentation
 
+class_name FreeLookCamera extends Camera3D
 # Modifier keys' speed multiplier
 const SHIFT_MULTIPLIER = 2.5
 const ALT_MULTIPLIER = 1.0 / SHIFT_MULTIPLIER
@@ -16,7 +17,7 @@ var _direction = Vector3(0.0, 0.0, 0.0)
 var _velocity = Vector3(0.0, 0.0, 0.0)
 var _acceleration = 30
 var _deceleration = -10
-var _vel_multiplier = 4
+var _vel_multiplier = 40
 
 # Keyboard state
 var _w = false
@@ -39,9 +40,9 @@ func _input(event):
 			MOUSE_BUTTON_RIGHT: # Only allows rotation if right click down
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
 			MOUSE_BUTTON_WHEEL_UP: # Increases max velocity
-				_vel_multiplier = clamp(_vel_multiplier * 1.1, 0.2, 20)
+				_vel_multiplier = clamp(_vel_multiplier * 1.1, 0.2, 200)
 			MOUSE_BUTTON_WHEEL_DOWN: # Decereases max velocity
-				_vel_multiplier = clamp(_vel_multiplier / 1.1, 0.2, 20)
+				_vel_multiplier = clamp(_vel_multiplier / 1.1, 0.2, 200)
 
 	# Receives key input
 	if event is InputEventKey:
